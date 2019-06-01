6dfd9d5f14e1117c070002d0aa88c5ed93e636d7
Pushkar N Kulkarni <pushkar.nk@in.ibm.com>
2019-06-01 03:03:21 +0000

    Correct the checks that trigger decompression
    The change introduced in pull request #18 made two wrong checks while deciding
    if a received frame has compressed data and must undergo decompression.
    
    First, we included continuation frames in the concept of a `data frame`. RFC6455
    clearly defines data frames as either text or binary frames, and continuation
    frames aren't treated as data frames, though they carry fragmented data.
    
    Secondly, RFC7692 clearly states that a message whose first fragment has rsv1
    set is considered as a compressed message. This implies that the continuation
    frames which carry subsequent fragments of a compressed message need not have
    rsv1 set.
    
    Overall, a frame is considered to be compressed if:
    1. It is a data frame and has rsv1 set
    2. It is a continuation frame received after a compressed data frame

--- Sources/KituraWebSocket/PermessageDeflateCompressor.swift
+++ Sources/KituraWebSocket/PermessageDeflateCompressor.swift
@@ -49,7 +49,7 @@ class PermessageDeflateCompressor : ChannelOutboundHandler {
         var frame = unwrapOutboundIn(data)
 
         // If this is a control frame, do not attempt compression.
-        guard frame.isDataFrame else {
+        guard frame.isDataFrame || frame.isContinuationFrame else {
              context.writeAndFlush(self.wrapOutboundOut(frame)).whenComplete { _ in
                  promise?.succeed(())
              }
